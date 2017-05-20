721cb67a3bbe8c05d0e608622cf450b2bec99340
kevin viglucci <kviglucci@gmail.com>
2017-05-20 14:31:09 -0500

    nodemone ignore db.json

--- tools/dev.js
+++ tools/dev.js
@@ -26,7 +26,7 @@ bs.init({
 
 nodemon({
   script: 'index.js',
-  ignore: ['client/', 'public/']
+  ignore: ['client/', 'public/', 'db.json']
 }).on('restart', files => {
   if (files) {
     const filenames = files.map(f => relativePath('', f)).join(', ');
