e94291693e32483e27422dbc0d8389d468d18b6d
kevin viglucci <kviglucci@gmail.com>
2017-05-31 01:04:30 -0500

    create NewReminderForm component and add needed inputs in modal

--- client/scripts/components/NewReminderForm.js
+++ client/scripts/components/NewReminderForm.js
@@ -5,31 +5,87 @@ import { bindActionCreators } from "redux";
 import { loadReminders } from "../actions/reminders";
 import ReminderListItem from "./ReminderListItem";
 
-import FlatButton from 'material-ui/FlatButton';
+import ContentAddIcon from "material-ui/svg-icons/content/add";
+import FloatingActionButton from "material-ui/FloatingActionButton";
+import FlatButton from "material-ui/FlatButton";
+import Dialog from "material-ui/Dialog";
+import DatePicker from "material-ui/DatePicker";
+import TextField from "material-ui/TextField";
+import TimePicker from "material-ui/TimePicker";
 
 class NewReminderForm extends Component {
 
-	componentWillMount () {
-	}
+	state = {
+		open: false,
+		nameInputValue: null,
+		dateInputValue: null,
+		timeInputValue: null
+	};
+
+	handleOpen = () => {
+		this.setState({ open: true });
+	};
+
+	handleClose = () => {
+		this.setState({ open: false });
+	};
+
+	handleNameInput = (event, value) => {
+		this.setState({ nameInputValue: value });
+	};
+
+	handleDateInput = (event, value) => {
+		this.setState({ dateInputValue: value });
+	};
+
+	handleTimeInput = (event, value) => {
+		this.setState({ timeInputValue: value });
+	};
 
 	render () {
+
+		let floatingButtonStyle = { position: "absolute", right: 40, bottom: 40 };
+		const cancelButton = <FlatButton label="Cancel" primary={true} onTouchTap={this.handleClose} />
+		const submitButton = <FlatButton label="Submit" primary={true} keyboardFocused={true} onTouchTap={this.handleClose} />
+		const actions = [cancelButton, submitButton];
+
 		return (
 			<div className="NewReminderForm">
-				<form className="mui-form--inline">
-					<legend>New Reminder</legend>
-					<div className="mui-textfield mui-textfield--float-label">
-						<input type="text"/>
-						<label>Title</label>
+				<FloatingActionButton
+					mini={false}
+					style={floatingButtonStyle}
+					onTouchTap={this.handleOpen}
+					className="NewReminderForm-openButton">
+					<ContentAddIcon className="NewReminderForm-openButton-icon" />
+				</FloatingActionButton>
+				<Dialog
+					title="New Reminder"
+					actions={actions}
+					modal={true}
+					open={this.state.open}
+					onRequestClose={this.handleClose}
+					className="NewReminderForm-dialog">
+					<div className="mui-row">
+						<div className="mui-col-md-12">
+							<TextField hintText="Name" value={this.state.nameInputValue} onChange={this.handleNameInput} />
+						</div>
 					</div>
-					<FlatButton label="Submit" primary={true} />
-				</form>
+					<div className="mui-row">
+						<div className="mui-col-md-6">
+							<DatePicker hintText="Date" value={this.state.dateInputValue} onChange={this.handleDateInput} />
+						</div>
+						<div className="mui-col-md-6">
+							<TimePicker hintText="Time" value={this.state.timeInputValue} onChange={this.handleTimeInput} />
+						</div>
+					</div>
+				</Dialog>
 			</div>
 		);
 	}
 }
 
-function mapStateToProps () {
-	return { };
+function mapStateToProps ({ reminders }) {
+	return { reminders };
 }
 
 function mapDispatchToProps (dispatch) {
--- client/scripts/components/ReminderList.js
+++ client/scripts/components/ReminderList.js
@@ -6,16 +6,7 @@ import { loadReminders } from "../actions/reminders";
 import ReminderListItem from "./ReminderListItem";
 import NewReminderForm from "./NewReminderForm";
 
-import ContentAddIcon from "material-ui/svg-icons/content/add";
-import FloatingActionButton from "material-ui/FloatingActionButton";
-import FlatButton from "material-ui/FlatButton";
-import Dialog from "material-ui/Dialog";
-
 class ReminderList extends Component {
-
-	state = {
-		open: true
-	};
 
 	componentWillMount () {
 		this.props.loadReminders();
@@ -27,50 +18,11 @@ class ReminderList extends Component {
 		);
 	}
 
-	handleOpen = () => {
-		this.setState({ open: true });
-	};
-
-	handleClose = () => {
-		this.setState({ open: false });
-	};
-
 	render () {
-		let floatingButtonStyle = { position: "absolute", right: 40, bottom: 40 };
-
-		const actions = [
-			<FlatButton
-				label="Cancel"
-				primary={true}
-				onTouchTap={this.handleClose}
-			/>,
-			<FlatButton
-				label="Submit"
-				primary={true}
-				keyboardFocused={true}
-				onTouchTap={this.handleClose}
-			/>,
-		];
-
 		return (
 			<div className="ReminderList mui-container">
-				<FloatingActionButton
-					mini={false}
-					style={floatingButtonStyle}
-					onTouchTap={this.handleOpen}>
-					<ContentAddIcon />
-				</FloatingActionButton>
-				<Dialog
-					title="New Reminder"
-					actions={actions}
-					modal={true}
-					open={this.state.open}
-					onRequestClose={this.handleClose}
-				>
-					The actions in this window were passed in as an array of React objects.
-				</Dialog>
 				<div className="ReminderList-newForm">
-					{/* <NewReminderForm></NewReminderForm> */}
+					<NewReminderForm></NewReminderForm>
 				</div>
 				<div className="ReminderList-items mui--z1">
 					{ this.renderItems() }
